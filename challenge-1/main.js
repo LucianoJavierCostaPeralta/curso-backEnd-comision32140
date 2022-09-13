01111bd3891d3f9e97b1892a4aa12af8f9c433b3
luciano <luchinjeje@gmail.com>
2022-09-13 17:36:38 -0300

    Challenge 1

---
+++ challenge-1/main.js
@@ -0,0 +1,42 @@
+class User {
+ constructor({ firstName, lastName, books, pets }) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.books = books;
+  this.pets = pets;
+ }
+
+ addBook = (bookName, author) => this.books.push({ bookName, author });
+ addPets = (pet) => this.pets.push(pet);
+ countPets = () => this.pets.length;
+ getBookName = () => {
+  const bookNameList = [];
+  this.books.map((book) => bookNameList.push(book.bookName));
+  return bookNameList;
+ }
+
+ getFullName = () => `${this.firstName}  ${this.lastName}`;
+}
+
+let data = {
+ firstName: 'Luciano Javier',
+ lastName: 'Costa Peralta',
+ books: [
+  { bookName: 'js', author: 'Oscar' },
+  { bookName: 'c++', author: 'Luciano' },
+ ],
+
+ pets: ['Cat', 'Dog']
+}
+
+let users = new User(data);
+
+console.log(users.getFullName());
+
+users.addPets('monky');
+
+console.log(users.countPets());
+
+users.addBook('python', 'Kevin');
+
+console.table(users.getBookName());
